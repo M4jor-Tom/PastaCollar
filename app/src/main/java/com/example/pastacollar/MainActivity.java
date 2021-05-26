15b5fce22d3ed6a0d2b39eb47d65a4e6b3cfb833
Tom <tom-vautray@hotmail.fr>
2021-05-26 18:45:28 +0200

    Binding of mainTextView in MainActivity

--- app/src/main/java/com/example/pastacollar/MainActivity.java
+++ app/src/main/java/com/example/pastacollar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pastacollar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainTextView = (TextView)findViewById(R.id.mainTextView);
     }
 }
